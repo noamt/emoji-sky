eaabeecae0350393b3df91ec35d5e10104e2af10
noamt <noam@10ne.org>
2017-11-04 23:40:24 +0200

    Adding a twitter client so that we can start posting tweets. Also changed most normal spaces to ideographic spaces so the emojis are nicely evened out

--- main.go
+++ main.go
@@ -1,11 +1,16 @@
 package main
 
 import(
+  "os"
   "fmt"
   "time"
   "math/rand"
   "strings"
+  "github.com/dghubble/go-twitter/twitter"
+  "github.com/dghubble/oauth1"
 )
+
+const space = "\U00003000"
 
 const sun = "\U00002600"
 const moon = "\U0001F315"
@@ -18,7 +23,14 @@ var midSkyByDay = [4]string {"\U0001F985", "\U0001F54A", "\U0001F986", "\U0001F4
 var lowSkyByDay = [2]string {"\U0001F41D", "\U0001F98B"}
 
 func main() {
-  fmt.Printf("%s%s%s%s\n\n%s%s", sunOrMoon(), clouds(), midSky(), midSky(), lowSky(), lowSky())
+  config := oauth1.NewConfig(os.Getenv("CONSUMER_KEYS"), os.Getenv("CONSUMER_SECRET"))
+  token := oauth1.NewToken(os.Getenv("ACCESS_TOKEN"), os.Getenv("ACCESS_SECRET"))
+  // http.Client will automatically authorize Requests
+  httpClient := config.Client(oauth1.NoContext, token)
+
+  // twitter client
+  client := twitter.NewClient(httpClient)
+  client.Statuses.Update(fmt.Sprintf("%s%s%s%s\n\n%s%s", sunOrMoon(), clouds(), midSky(), midSky(), lowSky(), lowSky()), nil)
 }
 
 func sunOrMoon() (string) {
@@ -33,7 +45,7 @@ func sunOrMoon() (string) {
 func clouds() (string) {
   r := newlySeededRandom()
   c := r.Intn(9)
-  cT := []string {" ", " ", " ", " ", " ", " ", " ", " ", " ", "\n"}
+  cT := []string {space, space, space, space, space, space, space, space, space, "\n"}
 
   for i := 0; i < c; i++ {
 		cT[r.Intn(9)] = cloud
@@ -45,7 +57,7 @@ func clouds() (string) {
 func midSky() (string) {
   r := newlySeededRandom()
   m := r.Intn(5)
-  mT := []string {" ", " ", " ", " ", " ", " ", " ", " ", " ", "\n"}
+  mT := []string {space, space, space, space, space, space, space, space, space, "\n"}
 
   for i := 0; i < m; i++ {
 		mT[r.Intn(9)] = midSkyByDay[r.Intn(4)]
@@ -57,7 +69,7 @@ func midSky() (string) {
 func lowSky() (string) {
   r := newlySeededRandom()
   l := r.Intn(5)
-  lT := []string {" ", " ", " ", " ", " ", " ", " ", " ", " ", "\n"}
+  lT := []string {space, space, space, space, space, space, space, space, space, "\n"}
 
   for i := 0; i < l; i++ {
 		lT[r.Intn(9)] = lowSkyByDay[r.Intn(2)]
